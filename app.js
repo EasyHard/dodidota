a2be1c47cfcfd221dd35bfe02a5217c2fa942584
easyhard <liuzhiyou.cs@gmail.com>
2014-03-08 00:56:04 +0800

    add production mode

--- app.js
+++ app.js
@@ -182,6 +182,9 @@ app.get('/account/unlink/:provider', passportConf.isAuthenticated, userControlle
 /**
  * Start Express server.
  */
+if (app.settings.env === 'production') {
+    app.set('port', 80);
+}
 
 app.listen(app.get('port'), function() {
   console.log("✔ Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
